572d6d0911b5301d7ad0697c6192e46e4d35a52b
bhupinderr8 <bhupinder121199@gmail.com>
2019-11-15 11:20:19 +0530

    Initial commit

---
+++ app/src/main/java/com/example/inventory/MainActivity.java
@@ -0,0 +1,115 @@
+package com.example.inventory;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.database.Cursor;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AbsListView;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+
+import com.example.inventory.data.InventoryDbHelper;
+import com.example.inventory.data.StockItem;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    private final static String LOG_TAG = MainActivity.class.getCanonicalName();
+    InventoryDbHelper dbHelper;
+    StockCursorAdapter adapter;
+    int lastVisibleItem = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        dbHelper = new InventoryDbHelper(this);
+        final FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, DetailsActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        final ListView listView = (ListView) findViewById(R.id.list_view);
+        View emptyView = findViewById(R.id.empty_view);
+        listView.setEmptyView(emptyView);
+
+        Cursor cursor = dbHelper.readStock();
+
+        adapter = new StockCursorAdapter(this, cursor);
+        listView.setAdapter((ListAdapter) adapter);
+        listView.setOnScrollListener(new AbsListView.OnScrollListener() {
+            @Override
+            public void onScrollStateChanged(AbsListView view, int scrollState) {
+                if(scrollState == 0) return;
+                final int currentFirstVisibleItem = view.getFirstVisiblePosition();
+                if (currentFirstVisibleItem > lastVisibleItem) {
+                    fab.show();
+                } else if (currentFirstVisibleItem < lastVisibleItem) {
+                    fab.hide();
+                }
+                lastVisibleItem = currentFirstVisibleItem;
+            }
+
+            @Override
+            public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount, int totalItemCount) {
+
+            }
+        });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        adapter.swapCursor(dbHelper.readStock());
+    }
+
+    public void clickOnViewItem(long id) {
+        Intent intent = new Intent(this, DetailsActivity.class);
+        intent.putExtra("itemId", id);
+        startActivity(intent);
+    }
+
+    public void clickOnSale(long id, int quantity) {
+        dbHelper.sellOneItem(id, quantity);
+        adapter.swapCursor(dbHelper.readStock());
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_add_dummy_data:
+                // add dummy data for testing
+                addDummyData();
+                adapter.swapCursor(dbHelper.readStock());
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void addDummyData() {
+        StockItem gummibears = new StockItem(
+                "Bottle",
+                "10 Rs",
+                97,
+                "Creative",
+                "+91855701-8990",
+                "97@gmail.com",
+                "android.resource://com.example.inventory/drawable/bottle");
+        dbHelper.insertItem(gummibears);
+    }
+}
+
